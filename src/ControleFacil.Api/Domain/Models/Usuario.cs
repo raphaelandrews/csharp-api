d9cc59b7911c3e947232a11bc2a30afea2e4694b
Raphael Andrews <raphaelandrews95@gmail.com>
2024-01-01 21:10:51 -0300

    first commit

--- src/ControleFacil.Api/Domain/Models/Usuario.cs
+++ src/ControleFacil.Api/Domain/Models/Usuario.cs
@@ -1,9 +1,4 @@
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
-using ControleFacil.Api.Contract.Usuario;
 
 namespace ControleFacil.Api.Damain.Models
 {
